430e82ce6472a3569c21098f0aae0de823ce354c
maybe-adamant <maybe.adamant@gmail.com>
2026-04-18 10:40:56 -0400

    add nemesis to tartarus

--- src/mods/biomes/i_tartarus.lua
+++ src/mods/biomes/i_tartarus.lua
@@ -1,5 +1,6 @@
 local internal = RunDirectorBiomeControl_Internal
 
+internal.registerNPCControl({ id = "Nemesis", biome = "I", min = 4, max = 10 })
 internal.registerRoomControl({ id = "RatCatcher", type = "MiniBoss", biome = "I", roomKey = "I_MiniBoss01", label = "The Verminancer", min = 3, max = 7 })
 internal.registerRoomControl({ id = "GoldElemental", type = "MiniBoss", biome = "I", roomKey = "I_MiniBoss02", label = "Goldwrath", min = 3, max = 7 })
 
